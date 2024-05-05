d32600f55cd9f417762d9e4059eda131019d7db4
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-05-05 18:12:54 +0800

    update 23

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: i32) -> i32 {
+    let price = 2;
+    let price_per_kg = 1;
+    let price_per_kg_for_more_than_40 = 1;
+
+    if n > 40 {
+        return n * price_per_kg_for_more_than_40;
+    } else {
+        return n * price;
+    }
+}
 
 // Don't modify this function!
 #[test]
