21e7b2222e98b87a357c28b1daabe23a4d91d88a
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-05-08 20:37:20 +0800

    修复了hashmaps1.rs和hashmaps2.rs中的错误，使其正常运行。

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        /*
+        对于每场比赛，首先检查scores哈希表中是否已经存在该球队。
+        如果存在，则更新该球队的得分和失分；
+        如果不存在，则创建一个新的Team结构体，并将其插入到scores哈希表中。
+         */
+        if scores.contains_key(&team_1_name) {
+            let team = scores.get_mut(&team_1_name).unwrap();
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        } else {
+            let team = Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            };
+            scores.insert(team_1_name, team);
+        }
+        if scores.contains_key(&team_2_name) {
+            let team = scores.get_mut(&team_2_name).unwrap();
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        } else {
+            let team = Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            };
+            scores.insert(team_2_name, team);
+        }
     }
     scores
 }
@@ -84,3 +110,16 @@ mod tests {
         assert_eq!(team.goals_conceded, 2);
     }
 }
+
+/*
+这段代码是一个Rust程序，用于处理足球比赛的结果，并生成一个得分表。得分表是一个HashMap，其中键是球队的名字，值是一个包含进球数和失球数的结构体。
+
+首先，我们定义了一个名为`Team`的结构体，用于存储球队的进球数和失球数。然后，我们实现了一个名为`build_scores_table`的函数，该函数接受一个包含比赛结果的字符串，并返回一个得分表。
+
+在`build_scores_table`函数中，我们首先创建了一个空的得分表。然后，我们遍历输入字符串中的每一行，将每行的球队名字和进球数分别提取出来。接下来，我们将进球数添加到球队的进球数中，并将失球数添加到球队的失球数中。最后，我们将球队的名字和进球数失球数插入得分表中。
+
+在`tests`模块中，我们编写了一些测试用例来验证`build_scores_table`函数的正确性。这些测试用例首先创建了一个包含比赛结果的字符串，然后调用`build_scores_table`函数生成得分表，最后检查得分表中的数据是否符合预期。
+
+
+
+*/
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -31,12 +29,29 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    /*
+    这段代码定义了一个函数 `transformer` ，
+    接受一个元组类型的 `Vec` 作为输入，
+    元组包含一个 `String` 和一个 `Command` 。
+    函数会根据 `Command` 的不同对 `String` 进行处理，
+    然后返回处理后的 `String` 组成的 `Vec` 。
+
+    1. 创建一个空的 `Vec` 类型的 `output` 用于存储处理后的 `String` 。
+    2. 遍历输入的 `Vec` ，对每个元组中的 `String` 和 `Command` 进行处理。
+    3. 根据 `Command` 的类型，分别对 `String` 进行不同的处理：
+        - 如果是 `Uppercase` 类型的 `Command` ，将 `String` 转换为大写并加入到 `output` 中。
+        - 如果是 `Trim` 类型的 `Command` ，将 `String` 去除空格并加入到 `output` 中。
+        - 如果是 `Append` 类型的 `Command` ，将 `String` 复制n次"bar"后拼接，并加入到 `output` 中。
+    4. 返回处理后的 `output` 。
+    */
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => output.push(string.to_owned() + &"bar".repeat(*n)),
+            }
         }
         output
     }
@@ -45,7 +60,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
