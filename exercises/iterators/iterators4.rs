a25aacb3a1d24948b5d468889d79f018f00d2b18
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-05-26 19:57:20 +0800

    修复了多个文件中的错误，使程序正常运行。

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,6 +13,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    /// 这段代码是Rust语言中的一个表达式，
+    /// 用于计算从1到给定整数`num`的范围内所有整数的乘积。
+    /// `(1..=num)`是一个范围表达式，表示从1开始，到`num`（包括`num`）结束的整数序列。
+    /// `product()`是一个方法，用于计算序列中所有元素的乘积。
+    /// 这个表达式的结果是一个整数，表示从1到`num`的范围内所有整数的乘积。
+    /// 例如，如果`num`等于5，那么表达式的结果就是1乘以2乘以3乘以4乘以5，等于120。
+    (1..=num).product()
 }
 
 #[cfg(test)]
